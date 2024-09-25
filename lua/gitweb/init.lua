8e0012e58c1dcf1a4da43b74ccdab8a0f353f176
Andrew Konkol <andrewkonkol@fullstory.com>
2024-09-25 11:14:12 -0500

    Add browsing, docs

--- lua/gitweb/init.lua
+++ lua/gitweb/init.lua
@@ -12,7 +12,7 @@ local function get_repo_url()
   end
 
   Git_repo_results =  vim.fn.systemlist("git config --get remote." .. Remote .. ".url")[1]
-  return Git_repo_results:gsub("%:","/"):gsub("git%@","https%://"):gsub("%.git", "")
+  return Git_repo_results:gsub("%:","/"):gsub("git%@","https%:%/%/"):gsub("%.git", "")
 end
 
 local function get_revision()
@@ -37,17 +37,32 @@ local function get_line_anchors()
     return Line
 end
 
-function M.setup(opts)
-  opts = opts or {}
-
-  vim.keymap.set({"n", "v"}, "<Leader>h", function()
+local function create_github_url()
     Repo_url = get_repo_url()
     Revision = get_revision()
     Line_anchors = get_line_anchors()
     Path = get_path()
-    local url = "GitHubURL: " .. Repo_url .. "/blob/" .. Revision .. "/" .. Path .. Line_anchors
-    vim.print(url)
+    return Repo_url .. "/blob/" .. Revision .. "/" .. Path .. Line_anchors
+end
 
+local function open_browser(url)
+	if url ~= '' then
+	  vim.uv.spawn("open", {args = {url}})
+	end
+end
+
+function M.setup(opts)
+  opts = opts or {}
+
+  vim.keymap.set({"n", "v"}, "<Leader>u", function()
+    local url = create_github_url()
+    vim.print("GitHubURL: " ..url)
+    vim.fn.setreg('+', url)
+   end)
+
+  vim.keymap.set({"n", "v"}, "<Leader>b", function()
+    local url = create_github_url()
+    open_browser(url)
    end)
 end
 
